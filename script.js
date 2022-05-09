5d7b0c74af921316da6413c38354853a8a6e6ba2
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-05-09 23:45:26 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// step - 1
+const result = document.querySelector(".result");
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+const increase = document.querySelector(".increase");
+
+
+decrease.addEventListener("click", () => {
+          result.innerHTML--;
+          color();
+})
+reset.addEventListener("click", () => {
+          result.innerHTML = 0;
+          color();
+})
+increase.addEventListener("click", () => {
+          result.innerHTML++;
+          color();
+})
+
+
+// Color implement
+
+function color() {
+          if (result.innerHTML < 0) {
+                    result.style.color = "#f03e3e";
+          }
+          else if (result.innerHTML > 0) {
+                    result.style.color = "#37b24d";
+          }
+          else {
+                    result.style.color = "#ffffff"
+          }
+}
